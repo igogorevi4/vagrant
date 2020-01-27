fd6366229e189d5c0bd725fdfa30c97ff332aba5
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2020-01-27 22:42:24 +0000

    ssh key pair management

--- jenkins/run.server.sh
+++ jenkins/run.server.sh
@@ -1,45 +1,36 @@
 #/usr/bin/env bash
-    
-# apt-get update -y
-# apt-get upgrade -y
 
-echo "__________ Setting Jenkins key for package manager __________"
+echo "----- Setting Jenkins key for package manager"
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-echo "__________ Intallation Java & Jenkins __________"
+echo "----- Intallation Java & Jenkins"
 apt-get update
-# apt-get install -y openjdk-8-jre
 apt-get install -y openjdk-8-jdk
 apt-get install -y jenkins
 
-echo "__________ Timezone is being set up to Europe/Moscow __________"
+echo "----- Timezone is being set up to Europe/Moscow"
 timedatectl set-timezone Europe/Moscow
 
-echo "__________ Jenkins user ssh keys configuring __________"
+echo "----- Jenkins user ssh keys configuring"
 mkdir -p /var/lib/jenkins/.ssh
 chmod 700 /var/lib/jenkins/.ssh
 chown jenkins:jenkins /var/lib/jenkins/.ssh
-touch /var/lib/jenkins/.ssh/id_rsa
-echo $1 >> /var/lib/jenkins/.ssh/id_rsa
-chown jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
-chmod 600 /var/lib/jenkins/.ssh/id_rsa 
 
-echo "__________ Writing Jenkins IPs to /etc/hosts __________"
+echo "----- Writing Jenkins IPs to /etc/hosts"
 cat >> /etc/hosts <<EOF
 # jenkins servers
 192.168.50.4      jenkins-master
 192.168.50.5      jenkins-slave
 EOF
 
-echo "__________ Starting up Jenkins server __________"
+echo "----- Starting up Jenkins server"
 systemctl start jenkins.service
 systemctl enable jenkins.service
 
-echo "__________ Waiting for Jenkins started __________"
-sleep 1m
-
-echo "__________ Getting initial password __________"
-# JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+echo "----- Getting initial password"
 JENKINSPWD=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
-echo "Go to your web-browser and put this password to finalize Jenkins installation: $JENKINSPWD "
+echo "----- After jenkins's started go to your web-browser and put this password to finalize Jenkins installation: $JENKINSPWD"
+sleep 1m
+echo "----- "
+echo "----- Jenkins master started"
--- jenkins/run.slave.sh
+++ jenkins/run.slave.sh
@@ -1,39 +1,21 @@
 #/usr/bin/env bash
-    
-# apt-get update -y
-# apt-get upgrade -y
 
-# echo "__________ Setting Jenkins key for package manager __________"
-# wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-echo "__________ Intallation Java __________"
+echo "----- Intallation Java"
 apt-get update
 apt-get install -y openjdk-8-jdk
 
-echo "__________ Timezone is being set up to Europe/Moscow __________"
+echo "----- Timezone is being set up to Europe/Moscow"
 timedatectl set-timezone Europe/Moscow
 
-echo "__________ Creating and configuring jenkins user __________"
+echo "----- Creating and configuring jenkins user"
 useradd --system -U -d /var/lib/jenkins -m -s /bin/bash jenkins
 mkdir -p /var/lib/jenkins/.ssh
 chmod 700 /var/lib/jenkins/.ssh
 chown jenkins:jenkins /var/lib/jenkins/.ssh
-touch /var/lib/jenkins/.ssh/authorized_keys
-echo $1 >> /var/lib/jenkins/.ssh/authorized_keys
-chown -R jenkins:jenkins /var/lib/jenkins/.ssh/authorized_keys
-chmod -R 600 /var/lib/jenkins/.ssh/authorized_keys 
 
-echo "__________ Allowing Jenkins from jenkins-master IP __________" 
-sshd_config AllowUsers jenkins@192.168.50.4
-
-echo "__________ Writing Jenkins IPs to /etc/hosts __________"
+echo "----- Writing Jenkins IPs to /etc/hosts"
 cat >> /etc/hosts <<EOF
 # jenkins servers
 192.168.50.4      jenkins-master
 192.168.50.5      jenkins-slave
 EOF
-
-# copy the id_rsa.pub from jenkins master to jenkins slave authorized keys (do manually for now)
-# vi /var/lib/jenkins-slave/.ssh/authorized_keys
-#   copy jenkinsmasterhost:/var/lib/jenkins/ssh/id_rsa.pub
